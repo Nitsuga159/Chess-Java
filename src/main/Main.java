db706122c992f01d66253f76e8d64163e574840e
Agustin Romero <agusromero0815@gmail.com>
2023-02-22 23:41:58 -0300

    chess created

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,56 +1,33 @@
 package main;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.EventListener;
 
 import javax.swing.*;
 
-public class Main {
+import board.Board;
 
-	public static void main(String[] args) {
+public class Main {
+	
+	public static void main(String args[]) {
 		Window myWindow = new Window();
 		
-		myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		myWindow.setTitle("Chess");
+		myWindow.setIconImage(
+			new ImageIcon(
+				new Main().getClass().getResource("/chess-icon.png")
+			).getImage()
+		);
 
-	
-}
-
-class Window extends JFrame {
-	public Window() {
-		setBounds(200, 200, 800, 800);
-		setVisible(true);
+		myWindow.add(new Board());
 		
-		add(new Board());
-	}
+		
+		 
 	
-	private class Board extends JPanel {
-		public void paintComponent(Graphics g) {
-			super.paintComponent(g);
-			
-			Dimension windowSize = getSize();
-			
-			int width = (int) 800 / 8;
-			int height = (int) 800 / 8;
-			
-			Color white = Color.WHITE;
-			Color brown = new Color(130, 84, 20);
-			
-			for(int i = 0; i < 8; i++) {
-				Color colorSelected = i % 2 == 0 ? white : brown;
-				for(int j = 0; j < 8; j++) {
-					g.setColor(colorSelected);
-					Rectangle rect = new Rectangle(width * i, height * j, width, height);
-					Graphics2D g2d = (Graphics2D) g;
-					g2d.fill(rect);
-					
-					colorSelected = colorSelected == white ? brown : white;
-				}
-			}
-			
-		}
+		
+		myWindow.initWindow(Board.SIZE, Board.SIZE);
 	}
 }
-
-
-
 
